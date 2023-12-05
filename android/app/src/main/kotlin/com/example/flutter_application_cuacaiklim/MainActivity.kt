df6cca4020cb7072c5112ffd917b34abfdaf7f17
Octarifa Angela <octarifa.825210106@stu.untar.ac.id>
2023-12-05 10:46:24 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_cuacaiklim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_cuacaiklim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/key.dart
@@ -0,0 +1,3 @@
+const apiId = "89bac19f5dd13fbfb86b86c75b689686";
+
+const defaultCity =  "Jakarta";
